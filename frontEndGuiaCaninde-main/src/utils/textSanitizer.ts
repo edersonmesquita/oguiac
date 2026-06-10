b008e4881e4842f49b8f473455951f0cf0346081
edersonmesquita <67840191+edersonmesquita@users.noreply.github.com>
2026-06-09 23:39:37 -0300

    Delete frontEndGuiaCaninde-main directory

--- frontEndGuiaCaninde-main/src/utils/textSanitizer.ts
+++
@@ -1,6 +0,0 @@
-export function sanitizeShareText(value: string): string {
-  return value
-    .replace(/\uFFFD+/g, '') // remove replacement chars: �
-    .replace(/\s{2,}/g, ' ')
-    .trim();
-}
